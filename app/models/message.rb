29c609a1e32d48e8ddb89b6634004c8c4ad3685a
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-04-04 17:30:16 -0400

    conflict file fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,11 +12,7 @@ class Message < ApplicationRecord
     notify_recipients
     update_parent_room
     broadcast_append_to self.room 
-<<<<<<< HEAD
-    
-=======
     broadcast_to_home_page
->>>>>>> 38bfb89 (Recent Message Activity Stream)
   end
 
   # Before creating a new room, confirm the participant
