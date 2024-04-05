eb6f0869873c341e12fc0a45a6523024c970bd46
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-04-05 18:07:37 -0400

    Delete and Edit Messages fuction

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+    before_action :set_message, only: [:destroy, :edit, :update]
     def create
       # Create a new message associated with the current user
       @message = current_user.messages.create(
@@ -11,12 +12,50 @@ class MessagesController < ApplicationController
           turbo_stream.update('flash', partial: "shared/message_error", locals: {message: @message.errors.full_messages.join(", ")})
       end
     end
+
+    def destroy
+      @room = Room.find(params[:room_id])
+      @message = @room.messages.find(params[:id])
+      @message.destroy
+      redirect_to @room, notice: 'Message was successfully deleted.'
+    end
+
+    def edit
+    end
+
+    def update
+      if params[:message][:remove_attachments].present?
+        params[:message][:remove_attachments].each do |attachment_id|
+          attachment = @message.attachments.find(attachment_id)
+          attachment.purge # or attachment.purge_later for background removal
+        end
+      end
+    
+      # Update message body or any other non-attachment attribute
+      @message.update(body: msg_params[:body])
+    
+      # Add new attachments without removing existing ones
+      if msg_params[:attachments]
+        msg_params[:attachments].each do |new_attachment|
+          @message.attachments.attach(new_attachment)
+        end
+      end
+    
+      redirect_to @room, notice: 'Message was successfully updated.'
+
+    end
+    
     
     private
     
     # Strong parameters method to ensure only permitted attributes are allowed
     def msg_params
-      params.require(:message).permit(:body, attachments: [])
+      params.require(:message).permit(:body, attachments: [], remove_attachments: [])
+    end
+
+    def set_message
+      @room = Room.find(params[:room_id])
+      @message = @room.messages.find(params[:id])
     end
   end
   
