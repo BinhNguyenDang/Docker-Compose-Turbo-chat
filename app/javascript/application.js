27a19ef0fed9b20704025b2da76832f77dcde152
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-07-23 00:02:00 +0700

    Finished config setup + seed db and import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,6 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import "channels";
-import "./mention.js";
+import "mention";
 import "chartkick";
 import "Chart.bundle";
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+admin = User.create(email: 'admin@example.com',
+                   password: 'password',
+                   password_confirmation: 'password',
+                   username: 'admin',
+                   role: 'admin')
+
+admin.joined_rooms << Room.create(name: 'General', is_private: false)
